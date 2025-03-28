831ad614ecd77b97de5bf656715067ad6f2f96f4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:04:59 -0300

    Atvidade concluida Leet

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string Christian;
+Console.WriteLine("Escreva uma frase");
+Christian = Console.ReadLine()!.Replace("a" ,"4")
+.Replace("i" , "1")
+.Replace("s" , "5");
+Console.WriteLine($"{Christian}");
